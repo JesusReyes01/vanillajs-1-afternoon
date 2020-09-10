16e7f57b8838bfdc8fbbeaa0a1497c2037f02c9f
jesusreyes01 <jesusareyes01@gmail.com>
2020-09-10 02:03:27 -0500

    added working reset button to part 2

--- Part2/index.js
+++ Part2/index.js
@@ -6,4 +6,16 @@ function setCard() {
     card.style.color = colorInput.value;
 }
 
-
+function reset(){
+    // document.getElementById(diamonds).style.color = gray;
+    // document.getElementById(clubs).style.color = gray;
+    // document.getElementById(hearts).style.color = gray;
+    // document.getElementById(spades).style.color = gray;
+    // document.getElementById(colorInput).value = '';
+    document.querySelector('#idInput').value = '';
+    document.querySelector('#colorInput').value = '';
+    document.querySelector('#diamonds').style.color = '';
+    document.querySelector('#hearts').style.color = '';
+    document.querySelector('#spades').style.color = '';
+    document.querySelector('#clubs').style.color = '';
+}
